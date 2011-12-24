7437c9d53add4ad03e798f8d780a49b42fbb93dd
Victor Savkin <vic.savkin@gmail.com>
2011-12-23 21:05:17 -0500

    adds all basic data types including array

--- lib/typed_fields/declaration.rb
+++ lib/typed_fields/declaration.rb
@@ -1,9 +1,81 @@
-module TypedFields
-  module Declaration
-    def self.included clazz
-      clazz.extend ClassMethods
-    end
+require 'bigdecimal'
 
+module TypedFields
+  class ObjectType
+    def parse obj
+      obj
+    end
+  end
+  
+  class IntegerType
+    def parse obj
+      return nil if obj.nil?
+      obj.to_i
+    end
+  end
+
+  class DecimalType
+    def parse obj
+      return nil if obj.nil?
+      BigDecimal.new(obj)
+    end
+  end
+
+  class StringType
+    def parse obj
+      return nil if obj.nil?
+      obj.to_s
+    end
+  end
+
+  class BooleanType
+    def parse obj
+      return nil if obj.nil?
+      obj == "true"
+    end
+  end
+
+  class ArrayType
+    def initialize type
+      @type = type
+    end
+    
+    def parse obj
+      return nil if obj.nil?
+      obj.map{|o| @type.parse o}
+    end
+  end
+  
+  module ClassMethods
+    { :object => ObjectType,
+      :integer => IntegerType,
+      :decimal => DecimalType,
+      :string => StringType,
+      :boolean => BooleanType}.each do |method_name, type|
+
+      define_method method_name do |*params|
+        declare_fields params, type.new
+      end
+
+      array_method = "array_of_#{method_name}s"
+      define_method array_method do |*params|
+        declare_fields params, ArrayType.new(type.new)
+      end
+    end
+    
+    private
+    def declare_fields params, type
+      options = params.last.is_a?(Hash) ? params.pop : {}
+      options[:type] = type
+      
+      @fields_type_information ||= {}
+      params.each do |field_name|
+        @fields_type_information[field_name] = options
+      end
+    end
+  end
+
+  module InstanceMethods
     def initialize_fields params
       set_default_values
       params.each do |field_name, value|
@@ -11,18 +83,6 @@ module TypedFields
       end
     end
     
-    module ClassMethods
-      def object *params
-        options = params.last.is_a?(Hash) ? params.pop : {}
-        options[:type] = :object
-        
-        @fields_type_information ||= {}
-        params.each do |field_name|
-          @fields_type_information[field_name] = options
-        end
-      end
-    end
-
     private
 
     def saved_type_info
@@ -30,13 +90,26 @@ module TypedFields
     end
 
     def set_value field_name, value
-      instance_variable_set "@#{field_name.to_s}", value
+      type_info = saved_type_info[field_name]
+      return unless type_info 
+      type = type_info[:type]
+      parsed_value = type.parse(value)
+      instance_variable_set "@#{field_name.to_s}", parsed_value
     end
 
     def set_default_values
       saved_type_info.each do |field_name, options|
-        set_value field_name, options[:default]
+        default_value = options[:default]
+        default_value = default_value.call(self) if default_value.respond_to? :call
+        set_value field_name, default_value
       end
+    end
+  end
+       
+  module Declaration
+    def self.included clazz
+      clazz.send :include, InstanceMethods
+      clazz.extend ClassMethods
     end
   end
 end
--- spec/typed_fields/declaration_spec.rb
+++ spec/typed_fields/declaration_spec.rb
@@ -27,6 +27,12 @@ describe TypedFields::Declaration do
       f(:field1).should == "value1"
       f(:field2).should == "value2"
     end
+
+    it "should ignore fields that were not declared" do
+      clazz.object :field
+      object.initialize_fields :another_field => "value"
+      f(:another_field).should be_nil
+    end
   end
 
   context "default values" do
@@ -43,10 +49,63 @@ describe TypedFields::Declaration do
       f(:field1).should == "default"
       f(:field2).should be_nil
     end
+
+    it "should call default value when it is a block" do
+      clazz.object :field, :default => Proc.new {"default"}
+      object.initialize_fields({})
+      f(:field).should == "default"
+    end
+
+    it "should pass the object being constructed to a default block" do
+      default = double("default proc")
+      clazz.object :field, :default => default
+      
+      default.stub(:respond_to?){true}
+      default.should_receive(:call).with(object).and_return("default")
+
+      object.initialize_fields({})
+      f(:field).should == "default"
+    end
   end
 
-#  context "#generate_initialize"
+  context "types" do
+    it "should parse integers" do
+      clazz.integer :field
+      object.initialize_fields(:field => "10")
+      f(:field).should == 10
+    end
 
+    it "should parse decimals" do
+      clazz.decimal :field
+      object.initialize_fields(:field => "1.5")
+      f(:field).should == 1.5
+    end
+
+    it "should parse strings" do
+      clazz.string :field
+      object.initialize_fields(:field => 100)
+      f(:field).should == "100"
+    end
+
+    it "should parse booleans" do
+      clazz.boolean :field
+      object.initialize_fields(:field => "true")
+      f(:field).should == true
+    end
+
+    it "should parse an array of integers" do
+      clazz.array_of_integers :field
+      object.initialize_fields(:field => ["1", "2"])
+      f(:field).should == [1,2]
+    end
+    
+    it "should call custom's type constructor when custom type is specified" do
+    end
+  end
+
+  context "inheritance" do
+  end
+  
   private
 
   def f field_name
